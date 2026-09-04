1d7394c274080157fb68b18b8a190e2c13e228ff
aschuchter21 <alex.schuchter1@gmail.com>
2026-09-04 00:52:34 -0400

    Align MachineType with Galacticraft MachineLib 0.2 API

--- projects/machinelib/forge-backport/src/main/java/dev/galacticraft/machinelib/api/machine/MachineType.java
+++ projects/machinelib/forge-backport/src/main/java/dev/galacticraft/machinelib/api/machine/MachineType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2026 Team Galacticraft
+ * Copyright (c) 2021-2023 Team Galacticraft
  * MIT License
  */
 package dev.galacticraft.machinelib.api.machine;
@@ -17,24 +17,29 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import java.util.List;
 import java.util.function.Supplier;
 
-/** Source/binary-compatible server subset of MachineLib 0.3 MachineType. */
+/** MachineLib 0.2-compatible machine descriptor used by Galacticraft 1.20.1. */
 public interface MachineType<Machine extends MachineBlockEntity, Menu extends AbstractContainerMenu> {
     static <Machine extends MachineBlockEntity, Menu extends AbstractContainerMenu> MachineType<Machine, Menu> create(
             Block block, BlockEntityType<Machine> blockEntityType, MenuType<Menu> menuType,
-            List<MachineStatus> statusDomain, Supplier<MachineEnergyStorage> energySupplier,
-            Supplier<MachineItemStorage> itemSupplier) {
-        return create(block, blockEntityType, menuType, statusDomain, energySupplier, itemSupplier, MachineFluidStorage::empty);
+            Supplier<MachineEnergyStorage> energySupplier, Supplier<MachineItemStorage> itemSupplier) {
+        return new MachineTypeImpl<>(block, blockEntityType, menuType, energySupplier, itemSupplier, MachineFluidStorage::empty);
     }
 
     static <Machine extends MachineBlockEntity, Menu extends AbstractContainerMenu> MachineType<Machine, Menu> create(
             Block block, BlockEntityType<Machine> blockEntityType, MenuType<Menu> menuType,
-            List<MachineStatus> statusDomain, Supplier<MachineEnergyStorage> energySupplier,
-            Supplier<MachineItemStorage> itemSupplier, Supplier<MachineFluidStorage> fluidSupplier) {
-        if (statusDomain == null || statusDomain.isEmpty()) throw new IllegalArgumentException("Status domain cannot be empty");
-        return new MachineTypeImpl<>(block, blockEntityType, menuType, statusDomain, energySupplier, itemSupplier, fluidSupplier);
+            Supplier<MachineEnergyStorage> energySupplier, Supplier<MachineItemStorage> itemSupplier,
+            Supplier<MachineFluidStorage> fluidSupplier) {
+        return new MachineTypeImpl<>(block, blockEntityType, menuType, energySupplier, itemSupplier, fluidSupplier);
     }
 
-    List<MachineStatus> statusDomain();
+    /** Compatibility overload retained from the first Forge scaffold. */
+    static <Machine extends MachineBlockEntity, Menu extends AbstractContainerMenu> MachineType<Machine, Menu> create(
+            Block block, BlockEntityType<Machine> blockEntityType, MenuType<Menu> menuType,
+            List<MachineStatus> ignoredStatusDomain, Supplier<MachineEnergyStorage> energySupplier,
+            Supplier<MachineItemStorage> itemSupplier) {
+        return create(block, blockEntityType, menuType, energySupplier, itemSupplier);
+    }
+
     MachineEnergyStorage createEnergyStorage();
     MachineItemStorage createItemStorage();
     MachineFluidStorage createFluidStorage();
