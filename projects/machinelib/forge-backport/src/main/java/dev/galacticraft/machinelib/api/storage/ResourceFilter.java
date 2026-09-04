f41c91c9dfb80f2b064d27df19bed10d9e93a56b
aschuchter21 <alex.schuchter1@gmail.com>
2026-09-04 00:56:22 -0400

    Add MachineLib 0.2 ResourceFilter package compatibility

---
+++ projects/machinelib/forge-backport/src/main/java/dev/galacticraft/machinelib/api/storage/ResourceFilter.java
@@ -0,0 +1,7 @@
+/* Copyright (c) 2021-2023 Team Galacticraft - MIT License */
+package dev.galacticraft.machinelib.api.storage;
+
+/** Historical package alias used by Galacticraft 1.20.1. */
+@FunctionalInterface
+public interface ResourceFilter<Resource> extends dev.galacticraft.machinelib.api.filter.ResourceFilter<Resource> {
+}
