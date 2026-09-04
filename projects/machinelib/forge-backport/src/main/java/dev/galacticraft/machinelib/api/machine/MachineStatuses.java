a94263960e02e66dc3e1d2f1cbbaec831959b21d
aschuchter21 <alex.schuchter1@gmail.com>
2026-09-04 00:27:17 -0400

    Add MachineLib Forge builtin statuses

---
+++ projects/machinelib/forge-backport/src/main/java/dev/galacticraft/machinelib/api/machine/MachineStatuses.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021-2026 Team Galacticraft
+ * MIT License
+ */
+package dev.galacticraft.machinelib.api.machine;
+
+import net.minecraft.ChatFormatting;
+
+public final class MachineStatuses {
+    public static final MachineStatus NOT_ENOUGH_ENERGY = MachineStatus.create("status.machinelib.not_enough_energy", ChatFormatting.RED, MachineStatus.Type.MISSING_ENERGY);
+    public static final MachineStatus INVALID_RECIPE = MachineStatus.create("status.machinelib.invalid_recipe", ChatFormatting.RED, MachineStatus.Type.MISSING_ITEMS);
+    public static final MachineStatus OUTPUT_FULL = MachineStatus.create("status.machinelib.output_full", ChatFormatting.GOLD, MachineStatus.Type.OUTPUT_FULL);
+    public static final MachineStatus CAPACITOR_FULL = MachineStatus.create("status.machinelib.capacitor_full", ChatFormatting.GOLD, MachineStatus.Type.OUTPUT_FULL);
+    public static final MachineStatus IDLE = MachineStatus.create("status.machinelib.idle", ChatFormatting.GOLD, MachineStatus.Type.MISSING_RESOURCE);
+    public static final MachineStatus ACTIVE = MachineStatus.create("status.machinelib.active", ChatFormatting.GREEN, MachineStatus.Type.WORKING);
+    private MachineStatuses() {}
+}
