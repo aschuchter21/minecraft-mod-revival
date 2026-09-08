e8dba90a785ca980d3ccad9f4f4d363134ce315a
aschuchter21 <alex.schuchter1@gmail.com>
2026-09-08 00:25:37 -0400

    Port Electric Compressor machine overlay to Forge

---
+++ projects/galacticraft/forge-overlays/src/main/java/dev/galacticraft/mod/content/block/entity/machine/ElectricCompressorBlockEntity.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2019-2023 Team Galacticraft
+ * MIT License
+ */
+package dev.galacticraft.mod.content.block.entity.machine;
+
+import dev.galacticraft.machinelib.api.block.entity.BasicRecipeMachineBlockEntity;
+import dev.galacticraft.machinelib.api.machine.MachineStatus;
+import dev.galacticraft.machinelib.api.machine.MachineStatuses;
+import dev.galacticraft.machinelib.api.menu.RecipeMachineMenu;
+import dev.galacticraft.mod.Galacticraft;
+import dev.galacticraft.mod.content.GCMachineTypes;
+import dev.galacticraft.mod.machine.GCMachineStatuses;
+import dev.galacticraft.mod.recipe.CompressingRecipe;
+import dev.galacticraft.mod.recipe.GCRecipes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/** Forge 1.20.1 overlay for Galacticraft's electric compressor. */
+public class ElectricCompressorBlockEntity extends BasicRecipeMachineBlockEntity<Container, CompressingRecipe> {
+    public static final int CHARGE_SLOT = 0;
+    public static final int INPUT_SLOTS = 1;
+    public static final int INPUT_LENGTH = 9;
+    public static final int OUTPUT_SLOTS = INPUT_SLOTS + INPUT_LENGTH;
+    public static final int OUTPUT_LENGTH = 2;
+
+    public ElectricCompressorBlockEntity(BlockPos pos, BlockState state) {
+        super(GCMachineTypes.ELECTRIC_COMPRESSOR, pos, state, GCRecipes.COMPRESSING_TYPE,
+                INPUT_SLOTS, INPUT_LENGTH, OUTPUT_SLOTS, OUTPUT_LENGTH);
+    }
+
+    @Override
+    protected void tickConstant(@NotNull ServerLevel world, @NotNull BlockPos pos,
+                                @NotNull BlockState state, @NotNull ProfilerFiller profiler) {
+        super.tickConstant(world, pos, state, profiler);
+        this.chargeFromStack(CHARGE_SLOT);
+    }
+
+    @Override
+    public @NotNull MachineStatus tick(@NotNull ServerLevel level, @NotNull BlockPos pos,
+                                       @NotNull BlockState state, @NotNull ProfilerFiller profiler) {
+        CompressingRecipe recipe = this.getActiveRecipe();
+        if (recipe != null && this.getState().isActive()) {
+            int maxProgress = this.getProcessingTime(recipe);
+            if (this.getProgress() % (maxProgress / 5) == 0 && this.getProgress() > maxProgress / 2) {
+                level.playSound(null, this.getBlockPos(), SoundEvents.ANVIL_LAND, SoundSource.BLOCKS,
+                        0.5F, level.random.nextFloat() * 0.1F + 0.9F);
+            }
+        }
+        return super.tick(level, pos, state, profiler);
+    }
+
+    @Override
+    protected @NotNull MachineStatus workingStatus(CompressingRecipe recipe) {
+        return GCMachineStatuses.COMPRESSING;
+    }
+
+    @Override
+    protected @Nullable MachineStatus hasResourcesToWork() {
+        return this.energyStorage().canExtract(Galacticraft.CONFIG_MANAGER.get().electricCompressorEnergyConsumptionRate())
+                ? null : MachineStatuses.NOT_ENOUGH_ENERGY;
+    }
+
+    @Override
+    protected void extractResourcesToWork() {
+        this.energyStorage().extract(Galacticraft.CONFIG_MANAGER.get().electricCompressorEnergyConsumptionRate());
+    }
+
+    @Override
+    public int getProcessingTime(@NotNull CompressingRecipe recipe) {
+        return recipe.getTime();
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int syncId, Inventory inv, Player player) {
+        if (this.getSecurity().hasAccess(player)) {
+            return new RecipeMachineMenu<>(syncId, (ServerPlayer) player, this);
+        }
+        return null;
+    }
+}
