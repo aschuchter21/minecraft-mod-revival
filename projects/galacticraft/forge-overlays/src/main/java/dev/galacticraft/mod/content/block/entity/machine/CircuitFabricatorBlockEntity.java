7d8ce3520872ef1994824d3697cad360e068b4b7
aschuchter21 <alex.schuchter1@gmail.com>
2026-09-08 00:25:50 -0400

    Port Circuit Fabricator machine overlay to Forge

---
+++ projects/galacticraft/forge-overlays/src/main/java/dev/galacticraft/mod/content/block/entity/machine/CircuitFabricatorBlockEntity.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2019-2023 Team Galacticraft
+ * MIT License
+ */
+package dev.galacticraft.mod.content.block.entity.machine;
+
+import dev.galacticraft.machinelib.api.block.entity.RecipeMachineBlockEntity;
+import dev.galacticraft.machinelib.api.compat.vanilla.RecipeTestContainer;
+import dev.galacticraft.machinelib.api.machine.MachineStatus;
+import dev.galacticraft.machinelib.api.machine.MachineStatuses;
+import dev.galacticraft.machinelib.api.menu.RecipeMachineMenu;
+import dev.galacticraft.machinelib.api.storage.slot.ItemResourceSlot;
+import dev.galacticraft.mod.Galacticraft;
+import dev.galacticraft.mod.content.GCMachineTypes;
+import dev.galacticraft.mod.content.item.GCItems;
+import dev.galacticraft.mod.machine.GCMachineStatuses;
+import dev.galacticraft.mod.recipe.FabricationRecipe;
+import dev.galacticraft.mod.recipe.GCRecipes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/** Forge 1.20.1 overlay for Galacticraft's circuit fabricator. */
+public class CircuitFabricatorBlockEntity extends RecipeMachineBlockEntity<Container, FabricationRecipe> {
+    public static final int CHARGE_SLOT = 0;
+    public static final int DIAMOND_SLOT = 1;
+    public static final int SILICON_SLOT_1 = 2;
+    public static final int SILICON_SLOT_2 = 3;
+    public static final int REDSTONE_SLOT = 4;
+    public static final int INPUT_SLOT = 5;
+    public static final int OUTPUT_SLOT = 6;
+
+    private final Container craftingInv;
+
+    public CircuitFabricatorBlockEntity(BlockPos pos, BlockState state) {
+        super(GCMachineTypes.CIRCUIT_FABRICATOR, pos, state, GCRecipes.FABRICATION_TYPE);
+        this.craftingInv = RecipeTestContainer.create(this.itemStorage().getSlot(INPUT_SLOT));
+    }
+
+    @Override
+    public void tickConstant(@NotNull ServerLevel world, @NotNull BlockPos pos,
+                             @NotNull BlockState state, @NotNull ProfilerFiller profiler) {
+        super.tickConstant(world, pos, state, profiler);
+        profiler.push("charge");
+        this.chargeFromStack(CHARGE_SLOT);
+        profiler.pop();
+    }
+
+    @Override
+    protected @Nullable MachineStatus hasResourcesToWork() {
+        return this.energyStorage().canExtract(Galacticraft.CONFIG_MANAGER.get().circuitFabricatorEnergyConsumptionRate())
+                ? null : MachineStatuses.NOT_ENOUGH_ENERGY;
+    }
+
+    @Override
+    protected void extractResourcesToWork() {
+        this.energyStorage().extractExact(Galacticraft.CONFIG_MANAGER.get().circuitFabricatorEnergyConsumptionRate());
+    }
+
+    @Override
+    protected boolean canOutputStacks(@NotNull FabricationRecipe recipe) {
+        ItemStack output = recipe.getResultItem(this.level.registryAccess());
+        return this.itemStorage().getSlot(OUTPUT_SLOT).canInsert(output.getItem(), output.getTag(), output.getCount());
+    }
+
+    @Override
+    protected @NotNull Container craftingInv() {
+        return this.craftingInv;
+    }
+
+    @Override
+    protected void outputStacks(@NotNull FabricationRecipe recipe) {
+        ItemStack output = recipe.getResultItem(this.level.registryAccess());
+        this.itemStorage().getSlot(OUTPUT_SLOT).insert(output.getItem(), output.getTag(), output.getCount());
+    }
+
+    @Override
+    protected void extractCraftingMaterials(@NotNull FabricationRecipe recipe) {
+        NonNullList<ItemStack> remainder = recipe.getRemainingItems(this.craftingInv);
+        this.itemStorage().getSlot(DIAMOND_SLOT).extractOne();
+        this.itemStorage().getSlot(SILICON_SLOT_1).extractOne();
+        this.itemStorage().getSlot(SILICON_SLOT_2).extractOne();
+        this.itemStorage().getSlot(REDSTONE_SLOT).extractOne();
+
+        ItemResourceSlot input = this.itemStorage().getSlot(INPUT_SLOT);
+        input.extractOne();
+        if (input.isEmpty() && !remainder.isEmpty()) {
+            ItemStack itemStack = remainder.get(0);
+            if (!itemStack.isEmpty()) {
+                input.insert(itemStack.getItem(), itemStack.getTag(), itemStack.getCount());
+            }
+        }
+    }
+
+    @Override
+    protected @NotNull MachineStatus workingStatus(FabricationRecipe recipe) {
+        return GCMachineStatuses.FABRICATING;
+    }
+
+    @Override
+    protected @Nullable FabricationRecipe findValidRecipe(@NotNull Level world) {
+        if (this.itemStorage().getSlot(DIAMOND_SLOT).contains(Items.DIAMOND)
+                && this.itemStorage().getSlot(SILICON_SLOT_1).contains(GCItems.RAW_SILICON)
+                && this.itemStorage().getSlot(SILICON_SLOT_2).contains(GCItems.RAW_SILICON)
+                && this.itemStorage().getSlot(REDSTONE_SLOT).contains(Items.REDSTONE)) {
+            return super.findValidRecipe(world);
+        }
+        return null;
+    }
+
+    @Override
+    public int getProcessingTime(@NotNull FabricationRecipe recipe) {
+        return recipe.getProcessingTime();
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int syncId, Inventory inv, Player player) {
+        if (this.getSecurity().hasAccess(player)) {
+            return new RecipeMachineMenu<>(syncId, (ServerPlayer) player, this);
+        }
+        return null;
+    }
+}
