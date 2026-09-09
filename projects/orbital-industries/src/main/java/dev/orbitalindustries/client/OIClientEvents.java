46c9fb8ebee00a6205aff6f0b3c301d7da74367b
aschuchter21 <alex.schuchter1@gmail.com>
2026-09-09 00:25:11 -0400

    Fix Forge 1.20.1 Mission Control screen registration

--- projects/orbital-industries/src/main/java/dev/orbitalindustries/client/OIClientEvents.java
+++ projects/orbital-industries/src/main/java/dev/orbitalindustries/client/OIClientEvents.java
@@ -3,10 +3,11 @@ package dev.orbitalindustries.client;
 import dev.orbitalindustries.OrbitalIndustries;
 import dev.orbitalindustries.client.screen.MissionControlScreen;
 import dev.orbitalindustries.registry.OIMenus;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterMenuScreensEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = OrbitalIndustries.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class OIClientEvents {
@@ -14,7 +15,7 @@ public final class OIClientEvents {
     }
 
     @SubscribeEvent
-    public static void registerScreens(RegisterMenuScreensEvent event) {
-        event.register(OIMenus.MISSION_CONTROL.get(), MissionControlScreen::new);
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> MenuScreens.register(OIMenus.MISSION_CONTROL.get(), MissionControlScreen::new));
     }
 }
